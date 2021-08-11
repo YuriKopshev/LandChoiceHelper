8e63255c410fec0b09ad869058c810c994e41d77
“Yuri <yura.kopshev@gmail.com>
2021-08-11 08:46:47 +0400

    changed variables name

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -8,17 +8,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Введите ширину участка или '0' для завершения работы программы:");
-            int input1 = scanner.nextInt();
-            if (input1 == 0) {
+            int width = scanner.nextInt();
+            if (width== 0) {
                 break;
             }
             System.out.println("Введите длину участка:");
-            int input2 = scanner.nextInt();
+            int length = scanner.nextInt();
             System.out.println("Введите цену за квадратный метр для проверки честности сделки:");
-            int input3 = scanner.nextInt();
-            int s = Estate.areaCounter(input1, input2);
-            int sum = Estate.priceCounter(s, input3);
-            Estate setl1 = new Estate(input1, input2, sum);
+            int pricePerMetr = scanner.nextInt();
+            int squareSize = Estate.areaCounter(width, length);
+            int sum = Estate.priceCounter(squareSize, pricePerMetr);
+            Estate setl1 = new Estate(width, length, sum);
             setl1.checkFair();
         }
     }
